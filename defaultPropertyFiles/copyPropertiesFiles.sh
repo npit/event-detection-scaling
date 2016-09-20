984b674a49c6d7fc32a31192231f6b3102415c28
npit <pittarasnikif@gmail.com>
2016-09-20 15:59:58 +0300

    Update gitignore

--- defaultPropertyFiles/copyPropertiesFiles.sh
+++ defaultPropertyFiles/copyPropertiesFiles.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 BDEDIR="../"
 cp ./newscrawler_configuration.properties "$BDEDIR/BDERSSCrawler/res/"
+cp ./blogcrawler_configuration.properties "$BDEDIR/BDERSSCrawler/res/"
 cp ./news_urls.txt "$BDEDIR/BDERSSCrawler/res/"
 cp ./clustering.properties "$BDEDIR/BDECLustering/res/"
 cp ./location_extraction.properties "$BDEDIR/BDELocationExtraction/res/"
