bda55aea12ea7315614313959ee978abe5121b4c
npit <pittarasnikif@gmail.com>
2016-11-28 13:25:14 +0200

    Bugfix on tweet cleaning, update skel properties

--- BDELocationExtraction/src/main/java/gr/demokritos/iit/location/extraction/DefaultLocationExtractor.java
+++ BDELocationExtraction/src/main/java/gr/demokritos/iit/location/extraction/DefaultLocationExtractor.java
@@ -55,7 +55,8 @@ public class DefaultLocationExtractor implements ILocationExtractor {
         while (iter.hasNext()) {
             String token = iter.next().trim();
             String cleaned = token.replaceAll("[^Α-Ωα-ωa-zA-Z0-9άέίόώήύΐΪΊΆΈΏΌΉΎ. ]", "");
-		if(cleaned.isEmpty()){ ret.add(""); continue; }
+            if(cleaned == null){ ret.add(""); continue; }
+            if(cleaned.isEmpty()){ ret.add(""); continue; }
             if (cleaned.charAt(0) == '.') {
                 cleaned = cleaned.substring(1);
             }
